6a171f116e0080e9bd5200dd23c74fefd41deb4d
Gill <asguilift@hotmail.com>
2022-07-22 20:36:09 -0300

    Alter site name

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Men',
-  title: 'Men',
+  site_name: 'Non-Fungible Men',
+  title: 'Non-Fungible Men',
   description: 'The ordinary business Men joins the metaverse',
   locale: 'en',
 };
